90cc073caaf10aa2437efbaba23e3299f5f932c3
Héctor Pérez <hectorperez_11c@cenidet.edu.mx>
2024-06-11 12:13:16 -0600

    Add project files.

---
+++ WebApplication13/StackTraceHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication13
+{
+    public class StackTraceHandler : IHttpHandler
+    {
+        public bool IsReusable => throw new NotImplementedException();
+
+        public void ProcessRequest(HttpContext context)
+        {
+            try
+            {
+                Debug.WriteLine("Processing request");
+            }
+            catch (Exception ex)
+            {
+                // BAD: printing a stack trace back to the response
+                context.Response.Write(ex.ToString());
+                return;
+            }
+            //try
+            //{
+            //    Debug.WriteLine("Processing request");
+            //}
+            //catch (Exception ex)
+            //{
+            //    // GOOD: log the stack trace, and send back a non-revealing response
+            //    Debug.WriteLine("Exception occurred", ex);
+            //    context.Response.Write("Exception occurred");
+            //    return;
+            //}
+        }
+    }
+}
